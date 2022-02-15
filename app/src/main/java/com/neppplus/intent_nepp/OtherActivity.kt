53706d6be756b01ac711909743ee127b7257e85e
smin9408 <stmdaks888@naver.com>
2022-02-15 16:59:30 +0900

    finish() 로 이전 화면으로 복귀

--- app/src/main/java/com/neppplus/intent_nepp/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_nepp/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.neppplus.intent_nepp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+
+//            돌아가는 경우에는 Intent 활용 X
+//            val myIntentToMain = Intent(this, MainActivity::class.java)
+//            startActivity(myIntentToMain)
+
+            finish()
+
+        }
     }
 }
