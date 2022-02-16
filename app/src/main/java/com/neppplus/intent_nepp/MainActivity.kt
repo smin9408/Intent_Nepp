5a85d99cde25f6b0f17181daac0a946063356040
smin9408 <stmdaks888@naver.com>
2022-02-16 10:03:36 +0900

    화면 이동시, 결과를 얻으러 이동 (새 닉네임)  - 결과 받기 : startActivityForResult  - 어떤 결과?: REQ_CODE_NICKNAME = 1000

--- app/src/main/java/com/neppplus/intent_nepp/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_nepp/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    모든 함수에서 공유할 수 있는 변수. ( 멤버변수 or 전역변수 )
+    val REQ_CODE_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,6 +35,15 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("message", inputMessage)
 
             startActivity(myIntent)
+
+        }
+
+        btnEditNickname.setOnClickListener {
+
+            val myIntent = Intent(this, EditNicknameActivity::class.java)
+
+//            새로운 닉네임을 받아내러 (결과를 얻으러) 가는 동작.
+            startActivityForResult(myIntent, REQ_CODE_NICKNAME) // 숫자값으로, 닉네임을 받으러 간다고 구별하는데 사용.
 
         }
 
